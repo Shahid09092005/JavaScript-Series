20e8ed77fb2cc4750b7aab517405a300319bf8e5
Shahid Mansuri <140784065+Shahid09092005@users.noreply.github.com>
2025-11-29 22:15:52 +0530

    Implement forEach example
    Added a forEach example to loop through an array.

---
+++ Day2/forEach.js
@@ -0,0 +1,9 @@
+//Note: Used to loop through each element (does NOT return a value)
+/*
+Syntax : arr.forEach((element, index, array) => { ... });
+*/
+let nums = [1, 2, 3];
+
+nums.forEach(n => {
+  console.log(n); 
+});
